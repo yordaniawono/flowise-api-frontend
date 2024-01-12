867b53c4186eb870f7c32a9bb33f2f2446038fe4
yordani <hiyordani@gmail.com>
2024-01-12 00:07:03 -0500

    added flowise creds / refactored API Call Response

--- controllers/flowise.js
+++ controllers/flowise.js
@@ -3,12 +3,31 @@ export const createPrediction = async (req, res) => {
   console.log(message);
 
   try {
-    // Call the Flowise API endpoint here..
     const flowiseData = {
       question: message,
     };
 
-    res.status(200).json({ message: "Demo Response" });
+    // Call flowise endpoint
+    const response = await fetch(`${process.env.FLOWISE_URL}/api/v1/prediction/${process.env.FLOW_ID}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.FLOWISE_API_KEY}`
+      },
+      body: JSON.stringify(flowiseData)
+    });
+
+    const data = await response.json();
+    console.log(data);
+
+    // Accessing the 'text' property from the data object
+    const resultText = data.text;
+
+    if (resultText) {
+      res.status(200).json({ message: resultText });
+    } else {
+      res.status(200).json({ message: 'Text not found in the response' });
+    }
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: error.message });
